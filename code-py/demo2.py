4e0bc43cca3a5071d2c748569cfe8b6a60ccbd19
Deserve123 <161918162+Deserve123@users.noreply.github.com>
2026-08-29 23:30:36 +0800

    Update demo2.py

--- code-py/demo2.py
+++ code-py/demo2.py
@@ -7,7 +7,7 @@ accept_socket, client_info = server_socket.accept()
 # accept_socket.send(b'hello world')   #b 把string除汉字转换成二进制
 # data = accept_socket.recv(1024).decode('utf-8')
 # print(f'服务端收到 来自{client_info}客户端的信息:{data}')
-with open('./work_py/my.txt','wb') as dest_file:
+with open('my.txt','wb') as dest_file:
     while True:
         file_data = accept_socket.recv(8192)
         if len(file_data) == 0:
